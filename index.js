99ad841c16d2d7dc5898b6e1d3cfabb43458f197
Vinicius <castvn@proton.me>
2024-04-17 10:59:06 -0300

    load image and save to local storage

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const canvasImg = document.querySelector("#imageCanvas");
+
+let tempImg = localStorage.getItem("img");
+if(tempImg){
+    canvasImg.src = tempImg;
+}
+
+
 const uploadArea = document.querySelector('#uploadArea');
 uploadArea.addEventListener("click", (e) => {
     e.preventDefault();
@@ -5,5 +13,14 @@ uploadArea.addEventListener("click", (e) => {
 // upload here
 uploadArea.addEventListener("change", ({target}) => {
     console.log('OnChange')
-    console.log(target.files)
+    const file = target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+        var imgSrc = event.target.result;
+        canvasImg.src = imgSrc;
+        // console.log(event.target.result)
+        localStorage.setItem("img", imgSrc);
+        console.log("Ok")
+      });
+    reader.readAsDataURL(file);
 })
